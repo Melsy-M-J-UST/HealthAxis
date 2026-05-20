b3962846b09fdd51816bf0bfbc00a07e3787acda
Vishal-Unni-UST <310030@ust.com>
2026-05-20 10:01:34 +0530

    Folder Structure Added

---
+++ HealthAxis/Service/Impl/AppointmentService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthAxis.Service.Impl
+{
+    internal class AppointmentService
+    {
+    }
+}
---
+++ HealthAxisTest/ServiceTest/DoctorServiceTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthAxisTest.ServiceTest
+{
+    internal class DoctorServiceTest
+    {
+    }
+}
