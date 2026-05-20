48057384e0c541b234eeae30322c9be2df46b45d
310054 <310054@ust.com>
2026-05-20 22:49:22 +0530

    Declared Appointment Class Variables and created an enum for AppointmentStatus

--- HealthAxis/Models/Appointment.cs
+++ HealthAxis/Models/Appointment.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace HealthAxis.Models
 {
-    internal class Appointment
+    public class Appointment
     {
+        public int AppointmentId { get; set; }
+        public Patient Patient { get; set; } = null!;
+        public Doctor Doctor { get; set; } = null!;
+        public DateTime ScheduledDate { get; set; }
+        public string Slot { get; set; } = string.Empty;
+        public AppointmentStatus Status { get; set; } = AppointmentStatus.Pending;
+        public string CancellationReason { get; set; } = string.Empty;
+
+        public enum AppointmentStatus
+        {
+            Pending,
+            Confirmed,
+            Cancelled,
+            Completed
+        }
+
     }
 }
