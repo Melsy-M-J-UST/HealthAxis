7a250cda84f5d0633433d63887439d9a5c8fbd63
310054 <310054@ust.com>
2026-05-20 07:18:13 +0530

    Created Patient Class and Initialised the variables

---
+++ HealthAxis/Models/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthAxis.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string? FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? InsuranceId { get; set; }
+
+
+    }
+}
