104edc923c2b2c2b53066f9b5a2e6bb6f9c2deb3
310054 <310054@ust.com>
2026-05-20 09:42:36 +0530

    Declared Variables in Doctor Class

--- HealthAxis/Models/Doctor.cs
+++ HealthAxis/Models/Doctor.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace HealthAxis.Models
 {
-    internal class Doctor
+    public class Doctor
     {
+        public int DoctorId { get; set; }
+        public int DoctorName { get; set; }
+        public int Specialization { get; set; }
+        public int Experience { get; set; }
+        public int Fees { get; set; }
+        public bool IsPractising { get; set; }
+
+
     }
 }
