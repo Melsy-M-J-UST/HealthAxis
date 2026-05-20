ec43a9b8e0f49aaaa01a0f3a4a48b8bbcfcd5a66
Nandana Linson(UST <310053@ust.com>
2026-05-20 09:39:51 +0530

    Created - Doctor Class

--- HealthAxis/Models/Doctor.cs
+++ HealthAxis/Models/Doctor.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace HealthAxis.Models
 {
-    internal class Doctor
+    public class Doctor
     {
+        public int DoctorId { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public SpecialisationOption Specialisation { get; set; }
+
+        public int YearsOfExperience { get; set; }
+
+        public int ConsultationFee { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public enum SpecialisationOption
+        {
+            GeneralPractitioner,
+            Cardiologist,
+            Dermatologist,
+            Neurologist,
+            Pediatrician,
+            Psychiatrist,
+            OrthopedicSurgeon,
+            Gynecologist,
+            Oncologist,
+            Endocrinologist
+
+        }
+
+        public string GetProfileSummary()
+        {
+            return $"DoctorId: {DoctorId}, FullName: {FullName}, Specialisation: {Specialisation}, YearsOfExperience: {YearsOfExperience}, ConsultationFee: {ConsultationFee}, IsActive: {IsActive}";
+        }
     }
 }
