5a1db70ce432763c55075c44018ee8f1bdcce987
Nandana Linson(UST <310053@ust.com>
2026-05-20 09:55:23 +0530

    Created - HealthRecord Class

--- HealthAxis/Models/HealthRecord.cs
+++ HealthAxis/Models/HealthRecord.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace HealthAxis.Models
 {
-    internal class HealthRecord
+    public class HealthRecord
     {
+        public int HealthRecordId { get; set; }
+
+        public Patient Patient { get; set; } = null!;
+
+        public Doctor Doctor { get; set; } = null!;
+
+        public DateTime VisitDate { get; set; }
+
+        public string Diagnosis { get; set; } = string.Empty;
+
+        public string Prescription { get; set; } = string.Empty;
+
+        public string Notes { get; set; } = string.Empty;
+
+        public string GetRecordSummary()
+        {
+            return $"HealthRecordId: {HealthRecordId}, Patient: {Patient.GetProfileSummary}, Doctor: {Doctor.GetProfileSummary}, VisitDate: {VisitDate.ToShortDateString()}, Diagnosis: {Diagnosis}, Prescription: {Prescription}, Notes: {Notes}";
+        }
     }
 }
