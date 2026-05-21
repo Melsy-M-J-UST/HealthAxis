b904044453c6c83ed5f94d97bc691cfe4518b1a7
310054 <310054@ust.com>
2026-05-21 10:59:03 +0530

    Implemented Patient repository Interface

--- HealthAxis/Repository/IPatientRepository.cs
+++ HealthAxis/Repository/IPatientRepository.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Repository
 {
-    internal class IPatientRepository
+    public interface IPatientRepository
     {
+        List<Patient> GetAllPatients();
+        Patient? GetPatientById(int id);
+        Patient RegisterPatient(Patient patient);
     }
 }
