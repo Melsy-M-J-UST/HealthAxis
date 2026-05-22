6e9b352a5973914790143741b57b053be6aa8c07
Nandana Linson(UST <310053@ust.com>
2026-05-22 07:35:39 +0530

    Repository Layer - Patient

--- HealthAxis/Repositories/IPatientRepository.cs
+++ HealthAxis/Repositories/IPatientRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,8 @@ namespace HealthAxis.Repositories
 {
     public class IPatientRepository
     {
+        List<Patient> GetAllPatients();
+        Patient? GetPatientById(int id);
+        Patient RegisterPatient(Patient patient);
     }
 }
--- HealthAxis/Repositories/Impl/PatientRepository.cs
+++ HealthAxis/Repositories/Impl/PatientRepository.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using HealthAxis.Data;
+using HealthAxis.Exceptions;
+using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Repositories.Impl
 {
-    internal class PatientRepository
+    public class PatientRepository
     {
+        public Patient RegisterPatient(Patient patient)
+        {
+            Database.Patients.Add(patient);
+            Console.WriteLine("Patient Added successfully");
+            return patient;
+        }
+        public List<Patient> GetAllPatients()
+        {
+            return Database.Patients.ToList();
+        }
+
+        public Patient? GetPatientById(int patientid)
+        {
+            var patient = Database.Patients.FirstOrDefault(p => p.PatientId == patientid);//find also works but not with lambda expression
+            if (patient == null)
+            {
+                throw new PatientNotFoundException($"Patient with id {patientid} not registered.");
+            }
+            return patient;
+        }
     }
 }
