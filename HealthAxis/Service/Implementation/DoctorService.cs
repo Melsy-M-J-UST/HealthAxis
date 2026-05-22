a1500b9305659f678b11078314862725361a7122
310054 <310054@ust.com>
2026-05-22 12:41:55 +0530

    Implemented the Doctor Service and Interface

--- HealthAxis/Service/Implementation/DoctorService.cs
+++ HealthAxis/Service/Implementation/DoctorService.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using HealthAxis.Models;
+using HealthAxis.Repository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Service.Implementation
 {
-    internal class DoctorService
+    public class DoctorService
     {
+        private readonly IDoctorRepository _repository;
+
+        public DoctorService(IDoctorRepository _repository)
+        {
+            this._repository = _repository;
+        }
+        public Doctor AddDoctor(Doctor doctor)
+        {
+            return _repository.AddDoctor(doctor);
+        }
+
+        public List<Doctor> SearchDoctorBySpecialisation(Doctor.Specialisations specialisation)
+        {
+            return _repository.SearchDoctorBySpecialisation(specialisation);
+        }
+        public List<Doctor> GetAllDoctors()
+        {
+            return _repository.GetAllDoctors();
+        }
     }
 }
