bc65ae7bcbfedbde85339fbd68598496754e9425
310054 <310054@ust.com>
2026-05-22 12:31:56 +0530

    Implemented the Health Record Repository and Interface

--- HealthAxis/Repository/IHealthRecordRepository.cs
+++ HealthAxis/Repository/IHealthRecordRepository.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Repository
 {
-    internal class IHealthRecordRepository
+    public interface IHealthRecordRepository
     {
+        HealthRecord AddRecord(HealthRecord record);
+        List<HealthRecord> GetRecordsByPatient(int patientId);
+        List<HealthRecord> GetRecordsByDoctor(int doctorId);
     }
 }
--- HealthAxis/Repository/Implementation/HealthRecordRepository.cs
+++ HealthAxis/Repository/Implementation/HealthRecordRepository.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using HealthAxis.Data;
+using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Repository.Implementation
 {
-    internal class HealthRecordRepository
+    public class HealthRecordRepository: IHealthRecordRepository
     {
+        private readonly Database _Db;
+
+        public HealthRecordRepository(Database Db)
+        {
+            _Db = Db;
+        }
+
+        public HealthRecord AddRecord(HealthRecord record)
+        {
+            record.RecordId = _Db.GetNextHealthRecordId();
+            _Db.HealthRecords.Add(record);
+            return record;
+        }
+
+        public List<HealthRecord> GetRecordsByPatient(int patientId)
+        {
+            return _Db.HealthRecords
+                .Where(r => r.Patient.PatientId == patientId)
+                .ToList();
+        }
+
+        public List<HealthRecord> GetRecordsByDoctor(int doctorId)
+        {
+            return _Db.HealthRecords
+                .Where(r => r.Doctor.DoctorId == doctorId)
+                .ToList();
+        }
     }
 }
