84da8ad68f22d7f528f512a9a82e8530561cc916
310054 <310054@ust.com>
2026-05-22 12:01:03 +0530

    Implemented the Doctor Repository and Interface

--- HealthAxis/Repository/Implementation/DoctorRepository.cs
+++ HealthAxis/Repository/Implementation/DoctorRepository.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using HealthAxis.Data;
+using HealthAxis.Exceptions;
+using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Repository.Implementation
 {
-    internal class DoctorRepository
+    public class DoctorRepository : IDoctorRepository
     {
+        private readonly Database _Db;
+
+        public DoctorRepository(Database Db)
+        {
+            _Db = Db;
+        }
+        public Doctor AddDoctor(Doctor doctor)
+        {
+            _Db.Doctors.Add(doctor);
+            return doctor;
+        }
+        public List<Doctor> SearchDoctorBySpecialisation(Doctor.Specialisations specialisation)
+        {
+            var doctors = _Db.Doctors
+                .Where(doc => doc.Specialisation == specialisation)
+                .ToList();
+
+            if (!doctors.Any())
+            {
+                throw new DoctorNotFoundException("No doctors found with the given specialization.");
+            }
+
+            return doctors;
+        }
+        public List<Doctor> GetAllDoctors()
+        {
+            return _Db.Doctors.ToList();
+        }
     }
 }
