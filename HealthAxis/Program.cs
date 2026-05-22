00112728bdfd0632b157e7cd2a57e938b8a9fe43
Nandana Linson(UST <310053@ust.com>
2026-05-22 08:00:55 +0530

    Program.cs - Menu

--- HealthAxis/Program.cs
+++ HealthAxis/Program.cs
@@ -1,1 +1,101 @@
-﻿
+﻿using AppointmentPortal.ConsoleApp.Data;
+using AppointmentPortal.ConsoleApp.Exceptions;
+using AppointmentPortal.ConsoleApp.Models;
+using AppointmentPortal.ConsoleApp.Repositories;
+using AppointmentPortal.ConsoleApp.Repositories.Impl;
+using AppointmentPortal.ConsoleApp.Services;
+using AppointmentPortal.ConsoleApp.Services.Impl;
+using HealthAxis.Repositories;
+using HealthAxis.Repositories.Impl;
+using HealthAxis.Services;
+using HealthAxis.Services.Impl;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.RegularExpressions;
+using static HealthAxis.Repositories.Impl.HealthRecordRepository;
+
+var services = new ServiceCollection();
+services.AddSingleton<AppDbContext>();
+services.AddScoped<IPatientRepository, PatientRepository>();
+services.AddScoped<IDoctorRepository, DoctorRepository>();
+services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+services.AddScoped<IHealthRepository, HealthRepository>();
+
+
+services.AddScoped<IPatientService, PatientService>();
+services.AddScoped<IDoctorService, DoctorService>();
+services.AddScoped<IAppointmentService, AppointmentService>();
+services.AddScoped<IHealthRecordService, HealthRecordService>();
+
+var provider = services.BuildServiceProvider();
+
+var db = provider.GetRequiredService<AppDbContext>();
+IPatientService patientService = provider.GetRequiredService<IPatientService>();
+IDoctorService doctorService = provider.GetRequiredService<IDoctorService>();
+IAppointmentService appointmentService = provider.GetRequiredService<IAppointmentService>();
+IHealthRecordService healthRecordService = provider.GetRequiredService<IHealthRecordService>();
+
+while (true)
+{
+    Console.WriteLine();
+    Console.WriteLine("===== Appointment Portal =====");
+    Console.WriteLine("1. Register a new patient");
+    Console.WriteLine("2. Add a new doctor");
+    Console.WriteLine("3. Search doctors by specialisation");
+    Console.WriteLine("4. Book an appointment for a patient");
+    Console.WriteLine("5. View all appointments for a patient");
+    Console.WriteLine("6. Confirm, cancel, or complete an appointment");
+    Console.WriteLine("7. Add a health record after a completed appointment");
+    Console.WriteLine("8. View health history for a patient");
+    Console.WriteLine("9. View all patients");
+    Console.WriteLine("10. View all doctors");
+
+    Console.WriteLine("11. View upcoming confirmed appointments");
+    Console.WriteLine("12. Exit");
+    Console.Write("Choose an option: ");
+
+    var choice = Console.ReadLine();
+    Console.WriteLine();
+
+    switch (choice)
+    {
+        case "1":
+            RegisterPatient();
+            break;
+        case "2":
+            AddDoctor();
+            break;
+        case "3":
+            SearchDoctorsBySpecialisation();
+            break;
+        case "4":
+            //BookAppointment();
+            break;
+        case "5":
+            //ViewAppointmentsForPatient();
+            break;
+        case "6":
+            ConfirmCancelOrCompleteAppointment();
+            break;
+        case "7":
+            AddHealthRecord();
+            break;
+        case "8":
+            ViewHealthHistory();
+            break;
+        case "9":
+            ViewAllPatients();
+            break;
+        case "10":
+            ViewAllDoctors();
+            break;
+        case "11":
+            //ViewUpcomingConfirmedAppointments();
+            break;
+        case "12":
+            Console.WriteLine("Exiting application...");
+            return;
+        default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+    }
+}
