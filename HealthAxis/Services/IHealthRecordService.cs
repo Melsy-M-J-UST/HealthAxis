7bf7a11b6c0a413edc79a6bd913ceb8e9a4293a6
Nandana Linson(UST <310053@ust.com>
2026-05-22 07:46:13 +0530

    Service Layer - HealthRecord

--- HealthAxis/Services/IHealthRecordService.cs
+++ HealthAxis/Services/IHealthRecordService.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Services
 {
-    internal class IHealthRecordService
+    public interface IHealthRecordService
     {
+        HealthRecord AddRecord(HealthRecord record);
+
+        List<HealthRecord> GetRecordsByPatient(int patientId);
+
+        List<HealthRecord> GetRecordsByDoctor(int doctorId);
     }
 }
--- HealthAxis/Services/Impl/HealthRecordService.cs
+++ HealthAxis/Services/Impl/HealthRecordService.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Services.Impl
 {
-    internal class HealthRecordService
+    public class HealthRecordService : IHealthRecordService
     {
+        public HealthRecord AddRecord(HealthRecord record)
+        {
+
+            if (record == null)
+            {
+                throw new Exception("Health record cannot be null.");
+            }
+
+            if (string.IsNullOrWhiteSpace(record.Diagnosis))
+            {
+                throw new Exception("Diagnosis cannot be empty.");
+            }
+
+            _repository.AddRecord(record);
+            return record;
+        }
+
+        public List<HealthRecord> GetRecordsByPatient(int patientId)
+        {
+            return _repository
+                .GetRecordsByPatient(patientId)
+                .OrderByDescending(record => record.VisitDate)
+                .ToList();
+        }
+
+        public List<HealthRecord> GetRecordsByDoctor(int doctorId)
+        {
+            return _repository
+                .GetRecordsByDoctor(doctorId)
+                .OrderByDescending(record => record.VisitDate)
+                .ToList();
+        }
+
     }
 }
