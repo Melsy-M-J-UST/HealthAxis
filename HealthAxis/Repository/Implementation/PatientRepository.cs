a12506a0f4c4bfee73ce224522fae39d0caf9e61
310054 <310054@ust.com>
2026-05-22 11:34:21 +0530

    Implemented Patient Repository

--- HealthAxis/Repository/Implementation/PatientRepository.cs
+++ HealthAxis/Repository/Implementation/PatientRepository.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using HealthAxis.Data;
+using HealthAxis.Exceptions;
+using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Repository.Implementation
 {
-    internal class PatientRepository
+    public class PatientRepository : IPatientRepository
     {
+        private readonly Database _db;
+        public PatientRepository(Database db)
+        {
+            _db = db;
+        }
+
+        public List<Patient> GetAllPatients()
+        {
+            return _db.Patients.ToList();
+        }
+
+        public Patient? GetPatientById(int patientid)
+        {
+            var patient = _db.Patients.FirstOrDefault(p => p.PatientId == patientid);
+            if (patient == null)
+            {
+                throw new PatientNotFoundException($"Patient with id {patientid} not registered.");
+            }
+            return patient;
+        }
+
+        public Patient RegisterPatient(Patient patient)
+        {
+            _db.Patients.Add(patient);
+            Console.WriteLine("Patient Added successfully");
+            return patient;
+        }
     }
 }
