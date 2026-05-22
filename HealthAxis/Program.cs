93a4c5a4e43230c215152b7706c5b0499ac73c23
310054 <310054@ust.com>
2026-05-22 11:05:46 +0530

    Added DependencyInjection in Program.cs

--- HealthAxis/Program.cs
+++ HealthAxis/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using HealthAxis.Data;
+using HealthAxis.Repository;
+using HealthAxis.Repository.Implementation;
+using HealthAxis.Service;
+using HealthAxis.Service.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddSingleton<Database>();
+services.AddScoped<IPatientRepository, PatientRepository>();
+services.AddScoped<IDoctorRepository, DoctorRepository>();
+services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+services.AddScoped<IHealthRecordRepository, HealthRecordRepository>();
+
+
+services.AddScoped<IPatientService, PatientService>();
+services.AddScoped<IDoctorService, DoctorService>();
+services.AddScoped<IAppointmentService, AppointmentService>();
+services.AddScoped<IHealthRecordService, HealthRecordService>();
+
+var provider = services.BuildServiceProvider();
+
+var db = provider.GetRequiredService<Database>();
+IPatientService patientService = provider.GetRequiredService<IPatientService>();
+IDoctorService doctorService = provider.GetRequiredService<IDoctorService>();
+IAppointmentService appointmentService = provider.GetRequiredService<IAppointmentService>();
+IHealthRecordService healthRecordService = provider.GetRequiredService<IHealthRecordService>();
