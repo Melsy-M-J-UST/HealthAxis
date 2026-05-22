f65acad839e5c540378879604391526de6be46a3
310054 <310054@ust.com>
2026-05-22 13:15:52 +0530

    Implemented the Health Record Service and Interface

--- HealthAxis/Service/IHealthRecordService.cs
+++ HealthAxis/Service/IHealthRecordService.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Service
 {
-    internal class IHealthRecordService
+    public interface IHealthRecordService
     {
+        HealthRecord AddRecord(HealthRecord record);
+        List<HealthRecord> GetRecordsByPatient(int patientId);
+        List<HealthRecord> GetRecordsByDoctor(int doctorId);
     }
 }
--- HealthAxis/Service/Implementation/HealthRecordService.cs
+++ HealthAxis/Service/Implementation/HealthRecordService.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using HealthAxis.Models;
+using HealthAxis.Repository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Service.Implementation
 {
-    internal class HealthRecordService
+    public class HealthRecordService : IHealthRecordService
     {
+        private readonly IHealthRecordRepository _repository;
+
+        public HealthRecordService(IHealthRecordRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public HealthRecord AddRecord(HealthRecord record)
+        {
+
+            if (record == null)
+            {
+                throw new Exception("Health record cannot be null.");
+            }
+
+            if (string.IsNullOrWhiteSpace(record.Diagnosis))
+            {
+                throw new Exception("Diagnosis cannot be empty.");
+            }
+
+            _repository.AddRecord(record);
+            return record;
+        }
+
+        public List<HealthRecord> GetRecordsByPatient(int patientId)
+        {
+            return _repository.GetRecordsByPatient(patientId).OrderByDescending(record => record.VisitedDate).ToList();
+        }
+
+        public List<HealthRecord> GetRecordsByDoctor(int doctorId)
+        {
+            return _repository.GetRecordsByDoctor(doctorId).OrderByDescending(record => record.VisitedDate).ToList();
+        }
     }
 }
