0c5b55b2ccdd66c9a9743df574b16f7f0cff5bb1
Nandana Linson(UST <310053@ust.com>
2026-05-22 07:48:04 +0530

    Service Layer - Patient

--- HealthAxis/Services/Impl/PatientService.cs
+++ HealthAxis/Services/Impl/PatientService.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using HealthAxis.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HealthAxis.Services.Impl
 {
-    internal class PatientService
+    public class PatientService : IPatientService
     {
+        public List<Patient> GetAllPatients()
+        {
+            return _repository.GetAllPatients();
+        }
+
+        public Patient? GetPatientById(int patientId)
+        {
+            return _repository.GetPatientById(patientId);
+        }
+        public Patient RegisterPatient(Patient patient)
+        {
+            return _repository.RegisterPatient(patient);
+        }
     }
 }
